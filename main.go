fa66957d5edf7a9d69e8141565ce19b0d5fa40ec
Muflih Afif <muflihafif234@gmail.com>
2025-05-08 22:15:42 +0700

    menambahkan function main

--- main.go
+++ main.go
@@ -6,3 +6,12 @@ import (
     "github.com/gofiber/fiber/v2"
 )
 
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+}
