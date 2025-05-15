900b26ad1c6ad4389f3b5fc817257e14e0a19d76
Muflih Afif <muflihafif234@gmail.com>
2025-05-15 22:36:17 +0700

    latiahn variabel

--- latihan/latihan.go
+++ latihan/latihan.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Helllo, Afif!\nD4 Teknik Infortmatika\n")	
+	// fmt.Printf("Helllo, Afif!\nD4 Teknik Infortmatika\n") || Print untuk hello worold
+	
+	var nama string = "afif"
+	fmt.Println("Hello",  nama ) // Print untuk hello world
 }
