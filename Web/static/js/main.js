f5cbb3a3b8fd3ef9283ccba04b5924fddc1bd850
Muflih Afif <muflihafif234@gmail.com>
2025-05-23 19:51:16 +0700

    update js

--- Web/static/js/main.js
+++ Web/static/js/main.js
@@ -1,3 +1,16 @@
 function showAlert() {
 	alert("Halo! Ini dari JavaScript di Go + Fiber!");
+}const images = [
+  "/static/img/img1.png",
+  "/static/img/img2.png",
+  "/static/img/img3.png"
+];
+let currentIndex = 0;
+
+function changeImage() {
+  const img = document.getElementById("slideshow");
+  currentIndex = (currentIndex + 1) % images.length;
+  img.src = images[currentIndex];
 }
+
+setInterval(changeImage, 3000); // Ganti setiap 3 detik
