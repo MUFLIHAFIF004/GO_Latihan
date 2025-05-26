41ccbf96bf91b8cb76587ad3be4a83ec831a8f4b
Muflih Afif <muflihafif234@gmail.com>
2025-05-26 19:18:28 +0700

    membuat returtn c.sjon

---
+++ Fiber/c.Json/json.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type User struct {
+	Name    string `json:"name"`
+	Tempat   string `json:"email"`
+	Age     int    `json:"age"`
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(User{
+			Name:   "Afif",
+			Tempat: "Bandung",
+			Age:    21,
+		})
+	})
+
+	app.Listen(":3000")
+}
