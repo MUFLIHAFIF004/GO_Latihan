a4bd39316c5977316feb95d9cbb6351df33aef40
Muflih Afif <muflihafif234@gmail.com>
2025-05-27 10:13:19 +0700

    perbaharuan latihan REST API

--- Fiber/main.go
+++ Fiber/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
-    "log"
+	"fmt"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
+type User struct {
+	Name   string `json:"name"`
+	Tempat string `json:"tempat"`
+	Age    int    `json:"age"`
+}
+
 func main() {
-    app := fiber.New()
+	app := fiber.New()
+    
+    app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(User{
+			Name:   "Zakaruna",
+			Tempat: "Bandung",
+			Age:    21,
+		})
+	})
 
-    app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Post("/user", func(c *fiber.Ctx) error {
+		var user User
 
-    log.Fatal(app.Listen(":3000"))
+		if err := c.BodyParser(&user); err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Invalid JSON")
+		}
+
+		fmt.Println("Data diterima:", user) // Debug log
+
+		return c.JSON(fiber.Map{
+			"message": "User berhasil dibuat",
+			"data":    user,
+		})
+	})
+
+	app.Listen(":3000")
 }
